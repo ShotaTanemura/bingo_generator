8505baa0723c631ed4edc85b7355631ed12311ff
ShotaTanemura <shota0711tane@gmail.com>
2023-09-06 22:38:51 +0900

    42BG06 Unify the design of the parts of BINGO with cosmic images

--- event.js
+++ event.js
@@ -1,5 +1,10 @@
-const features = Array("A", "B", "C", "D", "E", "F", "G", "H", "I", "J");
-const colors = Array({"color": "red", "time": 0}, {"color": "blue", "time": 0}, {"color": "yellow", "time": 0}, {"color": "green", "time": 0});
+const features = Array(
+	{"feature":"windows派", "time": 0}, {"feature":"mac派", "time": 0}, {"feature":"vscoder", "time": 0}, {"feature":"vimmer", "time": 0}, {"feature":"社会人", "time": 0}, {"feature":"qiitaに投稿している", "time": 0}, 
+	{"feature":"空のレポジトリ提出経験あり", "time": 0}, {"feature":"libft終わってない", "time": 0}, {"feature":"海外経験あり", "time": 0}, {"feature":"second circle", "time": 0}, 
+	{"feature":"Philanthropist 称号保有者", "time": 0}, {"feature":"動物と暮らしている", "time": 0}, {"feature":"1か月以上校舎PCにログインしていない", "time": 0}, {"feature":"atcoder青色以上", "time": 0}, 
+	{"feature":"上京してきました", "time": 0}, {"feature":"TIG 経験者", "time": 0}, {"feature":"Rush reviewer", "time": 0}, {"feature":"A.G.U 未使用", "time": 0}, {"feature":"自作 PC 持ってます", "time": 0});
+const colors = Array({"color": "rgb(196, 10, 10)", "time": 0}, {"color": "rgb(38, 70, 120)", "time": 0}, {"color": "rgb(222, 164, 4)", "time": 0}, {"color": "rgb(30, 120, 30)", "time": 0});
+// const colors = Array({"color": "red", "time": 0}, {"color": "blue", "time": 0}, {"color": "yellow", "time": 0}, {"color": "green", "time": 0});
 const imagePath = "images/hand_good.png";
 
 function setColor() {
@@ -7,20 +12,24 @@ function setColor() {
 	var cols = document.getElementsByClassName('col');
 	
 	forEach.call(cols, function (col) {
-		candidates = getCandidates(colors);
+		candidates = getColorCandidates(colors);
 		if (col.getAttribute('id') != "center") {
 			col.style.backgroundColor = candidates[Math.floor(Math.random()*candidates.length)];
+			col.style.opacity = 0.9
 			for (let color of colors) {
 				if (col.style.backgroundColor == color.color) {
 					color.time += 1;
 					break ;
 				}
 			}
+		} else {
+			col.style.backgroundColor = "white";
+			col.style.opacity = 0.9
 		}
 	});
 }
 
-function getCandidates(list) {
+function getColorCandidates(list) {
 	var min = list[0].time;
 	var candidates = [];
 
@@ -35,12 +44,34 @@ function getCandidates(list) {
 	return candidates;
 }
 
+function getFeatureCandidates(list) {
+	var min = list[0].time;
+	var candidates = [];
+
+	for (let element of list) {
+		if (min > element.time)
+			min = element.time;
+	}
+	for (let element of list) {
+		if (min == element.time)
+			candidates.push(element.feature);
+	}
+	return candidates;
+}
+
 function setFeatures() {
 	const forEach = Array.prototype.forEach;
 	var cols = document.getElementsByClassName('col');
 
 	forEach.call(cols, function (col) {
-		col.firstElementChild.textContent = features[Math.floor(Math.random()*features.length)];
+		candidates = getFeatureCandidates(features);
+		col.firstElementChild.textContent = candidates[Math.floor(Math.random()*candidates.length)];
+		for (let feature of features) {
+			if (col.firstElementChild.textContent == feature.feature) {
+				feature.time += 1;
+				break ;
+			}
+		}
 	});
 }
 
